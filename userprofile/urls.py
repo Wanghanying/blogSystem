9f16688cf7b74dabb707a360cb6a014edec3eaa3
Wanghanying <chihiro_why@163.com>
2021-11-21 19:27:06 +0800

    增加用户注册功能

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # 用户登录
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    # 用户注册
+    path('register/', views.user_register, name='register'),
 ]
