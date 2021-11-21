fd9d2edc6be8d06855d5a0ac9f1fd0d6ef0880ac
Wanghanying <chihiro_why@163.com>
2021-11-21 14:16:29 +0800

    1. 使用Django的表单类增加提交新文章功能 2. 新增删除博客文章功能，并使用layer组件防止用户误操作以及csrf令牌防止CSRF攻击

--- article/urls.py
+++ article/urls.py
@@ -5,6 +5,10 @@ app_name = 'article'
 urlpatterns = [
     path('article_list/', views.article_list, name = 'article_list'),
     path('article_detail/<int:id>', views.article_detail, name = 'article_detail'),
-
+    path('article_create/', views.article_create, name = 'article_create'),
+    # 删除文章
+    #path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    # 安全删除文章
+    path('article_safe_delete/<int:id>/', views.article_safe_delete, name='article_safe_delete'),
 
 ]
--- article/views.py
+++ article/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from article.models import ArticlePost
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
 
 import markdown
 
@@ -32,3 +34,45 @@ def article_detail(request, id):
     context = {'article': article}
     # 载入模板，并返回context对象
     return render(request, 'article/detail.html', context)
+
+# 写文章的视图
+def article_create(request):
+    # 判断用户是否提交数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit=False)
+            # 指定数据库中 id=1 的用户为作者
+            # 如果你进行过删除数据表的操作，可能会找不到id=1的用户
+            # 此时请重新创建用户，并传入此用户的id
+            new_article.author = User.objects.get(id=1)
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 完成后返回到文章列表
+            return redirect("article:article_list")
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    # 如果用户请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文
+        context = { 'article_post_form': article_post_form }
+        # 返回模板
+        return render(request, 'article/create.html', context)
+
+# 删文章
+def article_safe_delete(request, id):
+    if request.method == 'POST':
+        # 根据 id 获取需要删除的文章
+        article = ArticlePost.objects.get(id=id)
+        # 调用.delete()方法删除文章
+        article.delete()
+        # 完成删除后返回文章列表
+        return redirect("article:article_list")
+    else:
+        return HttpResponse("仅允许POST请求")
