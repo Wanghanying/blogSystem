58b08ca64ff44949e8a999d8c43e62309898bbb9
Wanghanying <chihiro_why@163.com>
2021-11-27 17:28:21 +0800

    部署准备工作

--- blogSystem/settings.py
+++ blogSystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3izviwo1=r2ye8d^u@to7mxc3&u=hdq2w(88e7gie4+veq5ga6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,7 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
+#静态文件收集目录
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
